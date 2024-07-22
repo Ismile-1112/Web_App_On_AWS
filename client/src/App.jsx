76b3c638031e53073b9b7dd676ff2ba45adc193c
Vismay Parikh <vismayparikh1112@gmail.com>
2024-07-22 12:00:22 -0400

    add delivery section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,56 @@
+import { Route, Redirect, Switch } from "react-router-dom";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+
+// HOC
+import HomeLayoutHOC from "./HOC/Home.Hoc";
+import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
+import CheckoutLayoutHOC from "./HOC/Checkout.Hoc";
+
+// Pages
+import Home from "./Page/Home";
+import Overview from "./Page/Restaurant/Overview";
+import OrderOnline from "./Page/Restaurant/OrderOnline";
+import Reviews from "./Page/Restaurant/Reviews";
+import Checkout from "./Page/Checkout";
+import Menu from "./Page/Restaurant/Menu";
+import Photos from "./Page/Restaurant/Photos";
+import RedirectRestaurant from "./Page/Restaurant/Redirect";
+import GoogleAuth from "./Page/GoogleAuth";
+
+//redux action 
+import { getMySelf } from "./Redux/Reducer/User/user.action";
+
+// axios global settings
+if (localStorage.zomatoUser) {
+  const { token } = JSON.parse(localStorage.zomatoUser);
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (localStorage.zomatoUser) dispatch(getMySelf());
+  }, []);
+  return(
+    <>
+    <Route path="/" exact>
+      <Redirect to="/delivery" />
+    </Route>
+    <Route path="/restaurant/:id" exact component={RedirectRestaurant} />
+      
+      <HomeLayoutHOC path="/:type" exact component={Home} />
+
+      <HomeLayoutHOC path="/google/:token" exact component={GoogleAuth} />
+      
+      <RestaurantLayoutHOC path="/restaurant/:id/overview" exact component={Overview} />
+      <RestaurantLayoutHOC path="/restaurant/:id/order-online" exact component={OrderOnline} />
+      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Menu} />
+      <RestaurantLayoutHOC path="/restaurant/:id/reviews" exact component={Reviews} />
+      <RestaurantLayoutHOC path="/restaurant/:id/photos" exact component={Photos} />
+      <CheckoutLayoutHOC path="/checkout/orders" exact component={Checkout} />
+    </>
   );
 }
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
+
+import Store from "./Redux/store";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
